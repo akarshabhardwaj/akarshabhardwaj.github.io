99a8bb75d5442f2568380c17ffb24ac87036e41e
akarshabhardwaj <akarshabhardwaj7280976503@gmail.com>
2023-01-03 20:38:54 +0530

    portfolio 4

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ $(document).ready(function () {
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Java Developer", "Coder", "Problem Solver"],
+        strings: ["Front-End Developer", "Coder", "Problem Solver"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true,
